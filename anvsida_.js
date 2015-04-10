f997ef055d2ba7f79f90a9888cffe6b7f9ca506e
ofverstedt <ofverstedt@live.se>
2015-04-10 13:38:18 +0200

    upddatering: widgets dyker upp

--- anvsida_.js
+++ anvsida_.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
 
+	$("#1").click(function(){
+		
+		$('#bikeInfo').fadeToggle('fast');
+	});
+
+	$("#2").click(function(){
+		
+		$('#SocialMedia').fadeToggle('fast');
+	});
+
 
 	$('tr.header')
 		.click(function(){
 			$(this).siblings('.child-'+this.id).toggle('fast');
 		});
 	$('tr[class^=child]').hide().children('tr');
+
+
+
 });	
