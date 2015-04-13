544ae045f3c6f4ef2897a31fb98efb4e273363b4
ofverstedt <ofverstedt@live.se>
2015-04-13 11:58:51 +0200

    nytt med olika mappar

--- anvsida.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-
-	$('tr.header')
-		.click(function(){
-			$(this).siblings('.child-'+this.id).toggle('fast');
-		});
-	$('tr[class^=child]').hide().children('tr');
-});	
