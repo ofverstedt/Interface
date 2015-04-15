1ffaed1828d1659015121f4c27fb56ca9df8587a
filippaenander <ffilippae@hotmail.com>
2015-04-15 11:09:28 +0200

    .

---
+++ testform.php
@@ -0,0 +1,9 @@
+<form action="" method="post">
+First name 
+<input type="text" name="firstname">
+<br>
+Last name 
+<input type="text" name="lastname">
+<input type="submit" name="sent" value="Skicka" >
+</form>
+
